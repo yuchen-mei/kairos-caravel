61bb2876dceb19f4e38a313feb35192cc1d827ea
Jeffrey Yu <jeffreyy@stanford.edu>
2022-07-21 13:56:26 -0700

    Updated copy_user_files script.

--- copy_user_files.sh
+++ copy_user_files.sh
@@ -22,18 +22,17 @@ top="user_proj_example"
 gds_src="$build_dir/21-cadence-innovus-signoff/outputs/design-merged.gds"
 lef_src="$build_dir/21-cadence-innovus-signoff/outputs/design.lef"
 def_src="$build_dir/21-cadence-innovus-signoff/outputs/design.def.gz"
-rtl_src="/scratch/users/jeffreyy/kairos-caravel/verilog/rtl_kairos/kairos_top.v"
 gl_src="$build_dir/21-cadence-innovus-signoff/outputs/design.virtuoso.v"
 spef_src="$build_dir/21-cadence-innovus-signoff/outputs/design.spef.gz"
-# spi_src="$build_dir/*-gds2spice/outputs/design_extracted.spice"
+rtl_src="./verilog/rtl_kairos/kairos_top.v"
+sram_lef="$build_dir/7-sram/outputs/sky130_sram_1kbyte_1rw1r_32x256_8.lef"
 
 gds_dest="./gds/$top.gds"
 lef_dest="./lef/$top.lef"
 def_dest="./def/$top.def.gz"
-rtl_dest="./verilog/rtl/$top.v"
 gl_dest="./verilog/gl/$top.v"
 spef_dest="./spef/$top.spef.gz"
-# spi_dest="./spi/lvs/$top.spice"
+rtl_dest="./verilog/rtl/$top.v"
 
 echo "Copying $gds_src to $gds_dest"
 cp $gds_src $gds_dest
@@ -43,16 +42,13 @@ echo "Copying $def_src to $def_dest"
 cp $def_src $def_dest
 echo "Copying $gl_src to $gl_dest"
 cp $gl_src $gl_dest
-echo "Copying $rtl_src to $rtl_dest"
-cp $rtl_src $rtl_dest
 echo "Copying $spef_src to $spef_dest"
 cp $spef_src $spef_dest
-# echo "Copying $spi_src to $spi_dest"
-# cp $spi_src $spi_dest
+echo "Copying $rtl_src to $rtl_dest"
+cp $rtl_src $rtl_dest
+cp sram_lef ./lef/
 
 echo "Decompressing $def_dest"
 gunzip $def_dest
 echo "Decompressing $spef_dest"
 gunzip $spef_dest
-
-cp $build_dir/7-sram/outputs/sky130_sram_1kbyte_1rw1r_32x256_8.lef /farmshare/scratch/users/jeffreyy/kairos-caravel/lef/
